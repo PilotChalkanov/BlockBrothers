77128dc10514f6343c548cc2f9b66dcd3cf829c2
PilotChalkanov <nik.chalkanov@gmail.com>
2021-12-18 16:39:28 +0200

    Add vehicle and vendors models

---
+++ models/vendors.py
@@ -0,0 +1,10 @@
+from db import db
+
+
+class VendorModel(db.Model):
+    __tablename__ = "vendors"
+
+    id = db.Column(db.Integer, primary_key=True)
+    company = db.Column(db.String(100),nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    phone = db.Column(db.String(13), nullable=False)
